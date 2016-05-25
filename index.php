aa9396f3a9837760f1d449a4febceaf050e528d0
terapiaMan <hosting@404.ge>
2016-05-25 14:31:59 +0400

    print error array

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 require_once('studio404_weather.php'); 
 /* You Have To Change Only Url */
-$url = 'http://www.accuweather.com/en/ge/gudauta/166775/weather-forecast/166775';
+$url = 'http://www.accuweather.com/en/ge/tbilisi/171705/weather-forecast/171705'; // Tbilisi
 $tempPath = "temp/";
 $classArray = array(
 	"condition"=>"cond", 
@@ -9,9 +9,17 @@ $classArray = array(
 );
 $studio404_weather = new studio404_weather(); 
 $array = $studio404_weather->lunch($url, $tempPath, $classArray);
+/* Error message Array */
+$error = $studio404_weather->error;
 
 // print
-echo "Weather Condition: ".$array['condition']."<br />";
-echo "Weather in celsius: ".$array['celsius']."<br />";
-echo "Weather in Fahrenheit: ".studio404_weather::celToFahren($array['celsius'])."° <br />";
+if(empty($error)){// if error array is empty
+	echo "Weather Condition: ".$array['condition']."<br />";
+	echo "Weather in celsius: ".$array['celsius']."<br />";
+	echo "Weather in Fahrenheit: ".studio404_weather::celToFahren($array['celsius'])."° <br />";
+}else{
+	echo "<pre>";
+	print_r($error);
+	echo "</pre>";
+}
 ?>
