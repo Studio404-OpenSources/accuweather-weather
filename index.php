b05d6f55d46f8fdb7229855a49bdbe32b439f3d5
terapiaMan <hosting@404.ge>
2016-05-25 15:07:48 +0400

    make it more simple

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php 
 require_once('studio404_weather.php'); 
+
 /* You Have To Change Only Url */
-$url = 'http://www.accuweather.com/en/ge/tbilisi/171705/weather-forecast/171705'; // Tbilisi
-$tempPath = "temp/"; /* temporary file path, you clean it as many times as you with via cron job */
-$classArray = array( /* class array : "whatever you want" => classname */
-	"condition"=>"cond", 
-	"celsius"=>"temp"
-);
 $studio404_weather = new studio404_weather(); 
-$array = $studio404_weather->lunch($url, $tempPath, $classArray);
+$array = $studio404_weather->lunch(
+	'http://www.accuweather.com/en/ge/tbilisi/171705/weather-forecast/171705', 
+	'temp/', 
+	array(
+		"condition"=>"cond", 
+		"celsius"=>"temp"
+	)
+);
 $error = $studio404_weather->error; /* Error message Array */
 
 /* Print */
